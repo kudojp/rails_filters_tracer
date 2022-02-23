04c4094c7a5132e998e35033af8b6df96d0778c1
kudojp <heyjudejudejude1968@gmail.com>
2022-02-23 17:56:42 +0900

    Use filters_tracer, not rails_filters_tracer, as a main class name

---
+++ lib/filters_tracer.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require_relative "rails_filters_tracer/version"
+
+module FiltersTracer
+end
--- lib/rails_filters_tracer.rb
+++ lib/rails_filters_tracer.rb
@@ -1,8 +1,1 @@
-# frozen_string_literal: true
-
-require_relative "rails_filters_tracer/version"
-
-module RailsFiltersTracer
-  class Error < StandardError; end
-  # Your code goes here...
-end
+require "filter_tracer"
