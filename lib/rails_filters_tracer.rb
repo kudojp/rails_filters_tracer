eb5a4beba7ebf09ba33d3174e80b569d031f80d9
kudojp <heyjudejudejude1968@gmail.com>
2022-02-23 16:53:01 +0900

    First commit

---
+++ lib/rails_filters_tracer.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rails_filters_tracer/version"
+
+module RailsFiltersTracer
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rails_filters_tracer/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RailsFiltersTracer
+  VERSION = "0.1.0"
+end
