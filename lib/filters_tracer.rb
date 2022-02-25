c7b7d8a6f055c93aedf9ff0d816be1cef73cb396
kudojp <heyjudejudejude1968@gmail.com>
2022-02-25 20:08:27 +0900

    Move some methods out of Configration class to FiltersTracer module

--- lib/filters_tracer.rb
+++ lib/filters_tracer.rb
@@ -12,82 +12,82 @@ module FiltersTracer
     def initialize
       self.logger = defined?(::Rails) ? Rails.logger : Logger.new(STDOUT)
     end
+  end
 
-    class << self
-      # @example
-      #   FiltersTracer.configure do |c|
-      #     self.logger = MyCustomLogger.new
-      #   end
-      # @yield [c]
-      # @yieldparam [FiltersTracer::Configuration] config
-      # @return [void]
-      def configure
-        yield configuration
+  class << self
+    # @example
+    #   FiltersTracer.configure do |c|
+    #     self.logger = MyCustomLogger.new
+    #   end
+    # @yield [c]
+    # @yieldparam [FiltersTracer::Configuration] config
+    # @return [void]
+    def configure
+      yield configuration
+    end
+
+    # @return [FiltersTracer::Configuration]
+    def configuration
+      @configuration ||= Configuration.new
+    end
+
+    # @return [Logger]
+    def logger
+      configuration.logger
+    end
+
+    def register_controller(controller)
+      controller_klass = self.controller_class_from(controller) || return
+
+      unless controller_klass.method_defined?(:_process_action_callbacks)
+        logger.error "===== [Failure] #{controller_klass} is not a traceable controller ====="
+        logger.error "===== This is probably because either #{controller_klass} is not a Rails controller or because the current version of Rails is not compatible with 'rails_filters_tracer' gem."
+        next
       end
 
-      # @return [FiltersTracer::Configuration]
-      def configuration
-        @configuration ||= Configuration.new
-      end
-
-      # @return [Logger]
-      def logger
-        configuration.logger
-      end
-
-      def register_controller(controller)
-        controller_klass = self.controller_class_from(controller) || return
-
-        unless controller_klass.method_defined?(:_process_action_callbacks)
-          logger.error "===== [Failure] #{controller_klass} is not a traceable controller ====="
-          logger.error "===== This is probably because either #{controller_klass} is not a Rails controller or because the current version of Rails is not compatible with 'rails_filters_tracer' gem."
-          next
-        end
-
-        controller_klass.class_eval do
-          self.include ::NewRelic::Agent::MethodTracer
-          begin
-            self._process_action_callbacks().send(:chain).each do |callback|
-              case callback.raw_filter
-              when Symbol
-                self.add_method_tracer callback.raw_filter
-              end
+      controller_klass.class_eval do
+        self.include ::NewRelic::Agent::MethodTracer
+        begin
+          self._process_action_callbacks().send(:chain).each do |callback|
+            case callback.raw_filter
+            when Symbol
+              self.add_method_tracer callback.raw_filter
             end
-          rescue
-            logger.error "===== [Failure] Filters of actions in #{controller_str} would not be traced properly.  ====="
-            logger.error "===== This is probably because either the current version of Rails or NewRelic::Agent is not compatible with 'rails_filters_tracer' gem."
           end
-        end
-
-        logger.info "===== [Success] Filters of all actions in #{controller_klass} will be reported to the New Relic server ====="
-      end
-
-      def register_all_subcontrollers(controller)
-        controller_klass = self.controller_class_from(controller) || return
-
-        controller_klass.descendants.each do |controller|
-          self.register_controller(controller)
+        rescue
+          logger.error "===== [Failure] Filters of actions in #{controller_str} would not be traced properly.  ====="
+          logger.error "===== This is probably because either the current version of Rails or NewRelic::Agent is not compatible with 'rails_filters_tracer' gem."
         end
       end
 
-      private
+      logger.info "===== [Success] Filters of all actions in #{controller_klass} will be reported to the New Relic server ====="
+    end
 
-      def controller_class_from(controller)
-        case controller
-        when Class
-          return controller
-        when String, Symbol
-          begin
-            return controller.constantize
-          rescue NameError
-            logger.error "===== [Failure] Controller: '#{controller}' has not been found ====="
-          end
-        else
-          logger.error "===== [Failure] Could not identify a controller from #{controller}(#{controller.class}) ====="
-        end
+    def register_all_subcontrollers(controller)
+      controller_klass = self.controller_class_from(controller) || return
 
-        nil
+      controller_klass.descendants.each do |controller|
+        self.register_controller(controller)
       end
+    end
+
+    private
+
+    def controller_class_from(controller)
+      case controller
+      when Class
+        return controller
+      when String, Symbol
+        begin
+          return controller.constantize
+        rescue NameError
+          logger.error "===== [Failure] Controller: '#{controller}' has not been found ====="
+        end
+      else
+        logger.error "===== [Failure] Could not identify a controller from #{controller}(#{controller.class}) ====="
+      end
+
+      nil
     end
   end
 end
